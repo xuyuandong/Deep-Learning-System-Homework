39cfa64caa74f631940706d89c6e0b692132d697
xuyuandong <cs.xuyd@gmail.com>
2022-11-04 10:17:04 +0800

    add hw3

---
+++ hw3/build/CMakeFiles/ndarray_backend_cpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/yuandongx/Downloads/DLS/hw3/python/needle/backend_ndarray/ndarray_backend_cpu.cpython-39-darwin.so"
+  "/Users/yuandongx/Downloads/DLS/hw3/python/needle/backend_ndarray/ndarray_backend_cpu.pdb"
+  "CMakeFiles/ndarray_backend_cpu.dir/src/ndarray_backend_cpu.cc.o"
+  "CMakeFiles/ndarray_backend_cpu.dir/src/ndarray_backend_cpu.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ndarray_backend_cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
